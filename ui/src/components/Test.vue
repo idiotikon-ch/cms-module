4d4acb91977c771c0db2e24ae0192e02123a11da
Florian Matter <flmt@mailbox.org>
2025-08-13 09:49:22 +0200

    error handling

--- ui/src/components/Test.vue
+++ ui/src/components/Test.vue
@@ -1,12 +1,20 @@
 <template>
-  <div>le test</div>
+  <div>le test s</div>
   <span v-if="article">
     {{ article.title }}
+  </span>
+  <span v-else>
+    Article not found.
   </span>
 </template>
 <script setup lang="ts">
 const { findOne } = useStrapi()
 const article = ref()
-const response = await findOne("articles", "zgvkx2itjz59h320n2p36d3g")
-article.value = response.data
+try {
+  const response = await findOne("articles", "zgvkx2itjz59h320n2p36d3g")
+  article.value = response.data
+} catch (error) {
+  console.error("Error fetching article:", error)
+  article.value = null // Handle the case where the article does not exist
+}
 </script>
