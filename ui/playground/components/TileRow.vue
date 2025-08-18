fd6259800a729014d3a9720c31b6aa3027e554c2
Florian Matter <flmt@mailbox.org>
2025-08-18 15:18:43 +0200

    move helpers

--- ui/playground/components/TileRow.vue
+++ ui/playground/components/TileRow.vue
@@ -21,46 +21,17 @@
 
 <script setup lang="ts">
 import MarkdownIt from 'markdown-it'
-
-import type { Tile } from '~/types'
+import { fetchTile, fetchTileBySlug } from '../utils/tileFetchers';
 
 const { find } = useStrapi()
 
 const md = new MarkdownIt()
 
-const fetchTile = async (id: string, target: any) => {
-  try {
-    const response = await find<Tile>('tiles', {
-      filters: { slug: id },
-      populate: ["picture"]
-    });
-    target.value = response.data[0];
-  } catch (error) {
-    console.error(`Error fetching tile ${id}:`, error);
-    target.value = null;
-  }
-  console.log(`Fetched tile ${id}:`, target.value);
-}
+const tile = ref();
+const tile2 = ref();
 
-
-const fetchTileBySlug = async (slug, target) => {
-  try {
-    const response = await fetch(`http://localhost:1337/api/tile/${slug}`);
-    if (!response.ok) throw new Error('Network response was not ok');
-    const data = await response.json();
-    target.value = data;
-  } catch (error) {
-    console.error(`Error fetching tile by slug ${slug}:`, error);
-    target.value = null;
-  }
-};
-
-
-const tile = ref()
-const tile2 = ref()
-
-fetchTile("another-tile", tile)
-fetchTileBySlug("test-tile-1", tile2)
+fetchTile("another-tile", tile);
+fetchTile("test-tile-1", tile2);
 
 
 </script>
