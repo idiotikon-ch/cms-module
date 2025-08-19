3d01e3a46ae0d30dab3c9ca10b45b4dfa05a81df
Florian Matter <florian.matter@idiotikon.ch>
2025-08-19 11:39:49 +0200

    index

--- ui/playground/pages/index.vue
+++ ui/playground/pages/index.vue
@@ -1,9 +1,31 @@
-<script setup lang="ts">
-import TileRow from '~/components/TileRow.vue'
-</script>
-
 <template>
-    <v-container>
-        <TileRow></TileRow>
+    <v-container class="text-center">
+        <v-row>
+            <v-col>
+                <h1>Welcome to the Playground App</h1>
+                <p>
+                    This application is designed to showcase and test various components from the UI module.
+                </p>
+                <p>
+                    Use this environment to explore, debug, and refine components before integrating them into a main
+                    application.
+                </p>
+            </v-col>
+        </v-row>
     </v-container>
 </template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+h1 {
+    font-size: 2.5rem;
+    margin-bottom: 1rem;
+}
+
+p {
+    font-size: 1.2rem;
+    margin-bottom: 0.5rem;
+}
+</style>
