ceb3c19def94a13733fa68415432266982006ad6
Florian Matter <florian.matter@idiotikon.ch>
2025-08-19 09:48:55 +0200

    installModule

--- ui/playground/nuxt.config.ts
+++ ui/playground/nuxt.config.ts
@@ -1,5 +1,5 @@
 export default defineNuxtConfig({
-  modules: ["../src/module", "@nuxtjs/strapi", "@nuxtjs/device"],
+  modules: ["../src/module"],
   strapi: {},
   ssr: false,
   idiCmsModule: {},
--- ui/src/module.ts
+++ ui/src/module.ts
@@ -2,6 +2,7 @@ import { resolve } from "node:path"
 import {
   defineNuxtModule,
   addPlugin,
+  installModule,
   createResolver,
   addComponentsDir,
 } from "@nuxt/kit"
@@ -16,14 +17,14 @@ export default defineNuxtModule<ModuleOptions>({
   },
   // Default configuration options of the Nuxt module
   defaults: {},
-  setup(moduleOptions, nuxt) {
+  async setup(moduleOptions, nuxt) {
     const resolver = createResolver(import.meta.url)
-
     // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
     addComponentsDir({
       path: resolver.resolve("./components"),
       prefix: "idiCms",
     })
     addPlugin(resolver.resolve("./runtime/plugin"))
+    await installModule("@nuxtjs/device", { append: true })
   },
 })
