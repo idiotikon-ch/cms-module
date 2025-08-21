00ac3c0e5e71b2dfba633128b3c8659e008029ec
Florian Matter <florian.matter@idiotikon.ch>
2025-08-21 13:44:12 +0200

    magnifying glass for ImageTile

--- ui/src/components/ImageTile.vue
+++ ui/src/components/ImageTile.vue
@@ -1,12 +1,20 @@
 <template>
     <v-card @click="dialog = true" class="flex w-100 sidebar tile-card">
-        <v-img :src="`${cmsImagesUrl}${thumb.picture.url}`" cover class="tile-image"></v-img>
+
+        <template v-slot:default="">
+            <v-btn disabled color="black" v-show=true
+                :style="{ zIndex: 1000, bottom: '10%', right: '10%', opacity: 0.6 }" position="absolute" variant="flat"
+                :icon="mdiMagnifyExpand"></v-btn>
+            <v-img :src="`${cmsImagesUrl}${thumb.picture.url}`" cover class="tile-image"></v-img>
+        </template>
+
     </v-card>
     <idiCmsImageViewer :wordStory="wordstory" :images="[thumb]" v-model="dialog"></idiCmsImageViewer>
 </template>
 
 <script setup lang="ts">
 import { mdiMagnifyExpand } from '@mdi/js';
+// import { useDisplay } from "vuetify"
 import VueMarkdown from 'vue-markdown-render';
 
 const props = defineProps({
@@ -19,6 +27,9 @@ const props = defineProps({
         default: false
     }
 })
+
+// const { isMobileOrTablet } = useDisplay()
+
 const dialog = ref(false)
 const cmsImagesUrl = useRuntimeConfig().public.strapi.url
 </script>
