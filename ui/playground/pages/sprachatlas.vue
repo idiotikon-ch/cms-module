e8345944660aec4368091c5fdd3f109b7d9cfcf6
Florian Matter <florian.matter@idiotikon.ch>
2025-08-25 09:26:14 +0200

    cover in desc

--- ui/playground/pages/sprachatlas.vue
+++ ui/playground/pages/sprachatlas.vue
@@ -1,23 +1,26 @@
 <template>
     <v-row>
-        <v-col :key="text" cols="12">
+        <v-col :key="text" cols="12" lg="9">
             <div class="article-body">
                 <h1 v-if="text && text.title">{{ text.title }}</h1>
                 <idiCmsRichTextRenderer v-if="text && text.content" :content="text.content" />
             </div>
+
+            <div class="mt-5">
+                <idiCmsAccordion :content="accordionPanels" panelTitleBgImage="/tabheader.png">
+                    <template #panel-0>
+                        <v-img src="https://kleinersprachatlas.ch/images/allgemein/Inhaltsverzeichnis_KSDS.png"
+                            width="100%" height="400px" cover />
+                    </template>
+                </idiCmsAccordion>
+            </div>
+        </v-col>
+
+        <v-col v-if="tile" cols="12" md="6" lg="3">
+            <idiCmsImageTile :tile="tile" />
         </v-col>
     </v-row>
 
-    <v-row>
-        <v-col>
-            <idiCmsAccordion :content="accordionPanels" panelTitleBgImage="/tabheader.png">
-                <template #panel-0>
-                    <v-img src="https://kleinersprachatlas.ch/images/allgemein/Inhaltsverzeichnis_KSDS.png" width="100%"
-                        height="400px" cover />
-                </template>
-            </idiCmsAccordion>
-        </v-col>
-    </v-row>
 </template>
 
 <script setup lang="ts">
@@ -29,7 +32,10 @@ import { fetchTile, fetchDocument } from '../utils/tileFetchers';
 import { ref } from 'vue';
 
 const text = ref();
+const tile = ref()
+
 fetchDocument("texts", "sprachatlas", text, { content: { populate: "*" } });
+fetchTile("ksds-cover", tile);
 
 const accordionPanels = [
     {
