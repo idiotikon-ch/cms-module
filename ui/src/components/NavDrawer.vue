f40d68a3f5ad61e96f685ff523d025c3d70a7b2d
Florian Matter <florian.matter@idiotikon.ch>
2025-10-16 16:50:09 +0200

    navdrawer can be on the right

--- ui/src/components/NavDrawer.vue
+++ ui/src/components/NavDrawer.vue
@@ -1,6 +1,13 @@
 <template>
-    <v-navigation-drawer width="310" :temporary="mdAndDown" :scrim="mdAndDown" :style="{ padding: 0 }"
-        :model-value="open" @update:model-value="onToggle">
+    <v-navigation-drawer
+        width="310"
+        :temporary="mdAndDown"
+        :scrim="mdAndDown"
+        :style="{ padding: 0 }"
+        :model-value="open"
+        :location="location"
+        @update:model-value="onToggle"
+    >
         <v-list class="pa-0">
             <NavItems v-for="item in items" :key="item.rank || item.title" :item="item" @select="onSelect" />
         </v-list>
@@ -15,6 +22,7 @@ import NavItems from './NavItems.vue';
 const props = defineProps({
     items: { type: Array, default: () => [] },
     open: { type: Boolean, default: false },
+    location: { type: String, default: 'left' },
 });
 const emit = defineEmits(['toggle', 'select']);
 
