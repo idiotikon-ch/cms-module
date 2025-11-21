3444788cc15d414bbb63eadbaa3babc8f3ab61ac
Florian Matter <florian.matter@idiotikon.ch>
2025-11-21 14:01:11 +0100

    footnotes in markdown

--- ui/src/components/MarkdownRenderer.vue
+++ ui/src/components/MarkdownRenderer.vue
@@ -5,9 +5,11 @@
 <script setup lang="ts">
 import { computed } from 'vue'
 import MarkdownIt from 'markdown-it'
+import markdownItFootnote from 'markdown-it-footnote'
 
 const props = defineProps<{ content: string }>()
 
 const md = new MarkdownIt()
+md.use(markdownItFootnote)
 const compiled = computed(() => md.render(props.content || ''))
 </script>
