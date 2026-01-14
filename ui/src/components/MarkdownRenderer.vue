72b43702ff18ef81fab77253e2e60c15af42f10b
Florian Matter <florian.matter@idiotikon.ch>
2026-01-14 17:44:28 +0100

    markdown attrs

--- ui/src/components/MarkdownRenderer.vue
+++ ui/src/components/MarkdownRenderer.vue
@@ -6,10 +6,12 @@
 import { computed } from 'vue'
 import MarkdownIt from 'markdown-it'
 import markdownItFootnote from 'markdown-it-footnote'
+import markdownItAttrs from 'markdown-it-attrs'
 
 const props = defineProps<{ content: string }>()
 
 const md = new MarkdownIt()
 md.use(markdownItFootnote)
+md.use(markdownItAttrs)
 const compiled = computed(() => md.render(props.content || ''))
 </script>
