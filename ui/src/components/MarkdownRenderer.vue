fc8a3b42e3e5075dbe5e97be81bc82544bd9e3ac
Florian Matter <florian.matter@idiotikon.ch>
2026-01-29 14:59:21 +0100

    markdown in html + details filter

--- ui/src/components/MarkdownRenderer.vue
+++ ui/src/components/MarkdownRenderer.vue
@@ -7,11 +7,27 @@ import { computed } from 'vue'
 import MarkdownIt from 'markdown-it'
 import markdownItFootnote from 'markdown-it-footnote'
 import markdownItAttrs from 'markdown-it-attrs'
+import markdownItContainer from 'markdown-it-container'
 
 const props = defineProps<{ content: string }>()
 
-const md = new MarkdownIt()
+const md = new MarkdownIt({ html: true })
 md.use(markdownItFootnote)
 md.use(markdownItAttrs)
+md.use(markdownItContainer, 'details', {
+    validate: function (params) {
+        return params.trim().match(/^details\s+(.*)$/)
+    },
+    render: function (tokens, idx) {
+        const m = tokens[idx].info.trim().match(/^details\s+(.*)$/)
+        if (tokens[idx].nesting === 1) {
+            // opening tag
+            return '<details><summary>' + md.utils.escapeHtml(m[1]) + '</summary>'
+        } else {
+            // closing tag
+            return '</details>'
+        }
+    },
+})
 const compiled = computed(() => md.render(props.content || ''))
 </script>
