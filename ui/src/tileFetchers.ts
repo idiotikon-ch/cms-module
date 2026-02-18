2563c73e182b00f849c80bfa45a07ffae43427c9
Florian Matter <florian.matter@idiotikon.ch>
2026-02-18 09:38:04 +0100

    proper tileFetching

--- ui/src/tileFetchers.ts
+++ ui/src/tileFetchers.ts
@@ -5,39 +5,21 @@ import { useStrapi } from "#imports"
 export type Tile = ApiTileTile
 
 /**
- * Fetch a tile by its slug using the Strapi API.
+ * Fetch a tile by its slug using the Strapi `find` method.
  * @param slug - The slug of the tile to fetch.
  * @param target - A ref to store the fetched tile.
  */
-export const fetchTileBySlug = async (slug: string, target: any) => {
-  try {
-    const response = await fetch(`http://localhost:1337/api/tile/${slug}`)
-    if (!response.ok) throw new Error("Network response was not ok")
-    const data = await response.json()
-    target.value = data
-  } catch (error) {
-    console.error(`Error fetching tile by slug ${slug}:`, error)
-    target.value = null
-  }
-}
-
-/**
- * Fetch a tile by its ID using the Strapi `find` method.
- * @param id - The ID of the tile to fetch.
- * @param target - A ref to store the fetched tile.
- */
-export const fetchTile = async (id: string, target: any) => {
+export const fetchTile = async (slug: string, target: any) => {
   const { find } = useStrapi()
 
   try {
     const response = await find<Tile>("tiles", {
-      filters: { slug: id },
+      filters: { slug: slug },
       populate: ["picture"],
     })
     target.value = response.data[0]
   } catch (error) {
-    console.error(`Error fetching tile ${id}:`, error)
+    console.error(`Error fetching tile by slug ${slug}:`, error)
     target.value = null
   }
-  // console.log(`Fetched tile ${id}:`, target.value)
 }
