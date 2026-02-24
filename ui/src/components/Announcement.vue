7b381783bf2d4c49b2f89b26d0dc4f0e60fc176e
Florian Matter <florian.matter@idiotikon.ch>
2026-02-24 14:19:05 +0100

    better announcements

--- ui/src/components/Announcement.vue
+++ ui/src/components/Announcement.vue
@@ -21,29 +21,40 @@
         </v-row>
     </v-card>
 
-    <v-card v-if="teaserOnly" variant="elevated" class="white-border-tile teaser" border @click="navigateTo(url)">
-        <v-card-title :style="{ whiteSpace: 'normal' }" :class="detailView ? `text-h3` : `text-h5`">
+    <v-card @click="navigateTo(url)" variant="elevated" class="white-border-tile teaser" border>
+        <v-card-title :style="{ whiteSpace: 'normal' }">
             {{ title }}
         </v-card-title>
         <v-card-subtitle>
             {{ date }}
         </v-card-subtitle>
         <v-img v-if="picture" :src="`${cmsImagesUrl}${picture.url}`" cover></v-img>
-        <v-card-text v-if="content">
-            <BlockRenderer :content="teaserContent" :baseUrl="cmsImagesUrl" />
+        <v-card-text v-if="teaserText">
+            {{ teaserText }}
         </v-card-text>
         <div v-if="url" class="d-flex read-more">
-            <NuxtLink :to="url">Weiterlesen</NuxtLink>
+            <NuxtLink :to="url">{{ readMoreText }}</NuxtLink>
         </div>
+        <template v-else>
+            <v-card-title :style="{ whiteSpace: 'normal' }">
+                {{ title }}
+            </v-card-title>
+            <v-card-subtitle>
+                {{ date }}
+            </v-card-subtitle>
+            <v-img v-if="picture" :src="`${cmsImagesUrl}${picture.url}`" cover></v-img>
+            <v-card-text v-if="teaserText">
+                {{ teaserText }}
+            </v-card-text>
+        </template>
     </v-card>
 </template>
 
 <script lang="ts" setup>
 import BlockRenderer from './BlockRenderer.vue'
-import { useRuntimeConfig, computed } from '#imports'
-
-const runtimeConfig = useRuntimeConfig()
-const cmsImagesUrl: string = runtimeConfig.public.strapi.url
+import { computed } from 'vue'
+import { getPlainTxtTeaser } from '../utils/markdown'
+import { navigateTo } from '#imports'
 
 const props = defineProps({
     title: String,
@@ -54,24 +65,36 @@ const props = defineProps({
         required: true
     },
     url: String,
+    baseUrl: {
+        type: String,
+        default: ''
+    },
     teaserOnly: {
         type: Boolean,
         default: false
     },
+    teaserLength: {
+        type: Number,
+        default: 180
+    },
     detailView: {
         type: Boolean,
         default: false
+    },
+    readMoreText: {
+        type: String,
+        default: 'Weiterlesen'
     }
 })
 
-const teaserContent = computed(() => {
-    if (!props.content) return []
+const cmsImagesUrl = computed(() => props.baseUrl)
+
+const teaserText = computed(() => {
+    if (!props.content) return ''
     const markdownBlock = props.content.find(block => block.__component === 'shared.markdown-text')
     if (markdownBlock && markdownBlock.body) {
-        const text = markdownBlock.body
-        const truncatedBody = text.length > 180 ? text.substring(0, 177) + '...' : text
-        return [{ ...markdownBlock, body: truncatedBody }]
+        return getPlainTxtTeaser(markdownBlock.body, props.teaserLength)
     }
-    return []
+    return ''
 })
 </script>
